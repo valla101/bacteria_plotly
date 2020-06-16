82a8474a7c39bd349296984829e46b68793d102a
Alex Valladares <alex.valla101@gmail.com>
2020-06-15 23:13:57 -0400

    responsive panel and bar chart completed

--- static/js/app.js
+++ static/js/app.js
@@ -1,4 +1,4 @@
-d3.json("samples.json").then(function test(data){
+d3.json("samples.json").then(function init(data){
     // console.log(data);
     // Testing Data Output
     var entries = Object.entries(data);
@@ -21,10 +21,11 @@ d3.json("samples.json").then(function test(data){
     var slicedLabels = sample_object_entries[0].otu_ids.slice(0, 10);
     
     var reversedData = slicedData.reverse();
-    console.log(slicedData);
-    console.log(slicedLabels);
+    // console.log(slicedData);
+    // console.log(slicedLabels);
+    
     var sliced_labels = slicedLabels.map(item=>`OTU ID:${item}`);
-    console.log(sliced_labels);
+    // console.log(sliced_labels);
 
     var trace1 = {
         x: reversedData,
@@ -36,11 +37,31 @@ d3.json("samples.json").then(function test(data){
     var data = [trace1];
     
     var layout = {
-    height: 1000,
-    width: 1000,
+    height: 900,
+    width: 900,
     };
     
       Plotly.newPlot("bar", data, layout);
+
+      var trace2 = {
+        x: sliced_labels,
+        y: reversedData,
+        mode: 'markers',
+        // marker: {
+        //   size: [reversedData.min, reversedData.max]
+        // }
+      };
+      
+      var data2 = [trace2];
+      
+      var layout2 = {
+        title: 'Marker Size',
+        showlegend: false,
+        height: 900,
+        width: 900
+      };
+      
+      Plotly.newPlot('bubble', data2, layout2);
 
 // Adding MetaData to Panel
 
@@ -53,8 +74,101 @@ Object.entries(test_object).forEach(([key,value])=>{
 });
 
 
+
 });
 
+// d3.selectAll("#selDataset").on("change", optionChanged);
+
+function optionChanged(selectedData){
+    d3.json("samples.json").then(function (data){
+    
+    var entries = Object.entries(data);
+    var sample_metadata = entries[1][1];
+    var sample_object_entries = entries[2][1];
+    var sample_ids = entries[0][1];
+    console.log(selectedData);
+    console.log(sample_object_entries);
+
+    var i = 0;
+    var panel = d3.select("#sample-metadata");
+    panel.html("");
+    var x = [];
+    var y = [];
+
+    sample_ids.forEach((value)=>{
+        if (value === selectedData){
+            // console.log(i);
+            // Use i to slice entries and get all the data wanted in the panel
+            // append data to the panel
+            var test_object = sample_metadata[i];
+            Object.entries(test_object).forEach(([key,value])=>{
+                panel.append("p").text(`${key}:${value}`);
+    
+            });
+        }
+
+        
+
+            
+            i = i+1;
+    
+        });
+    sample_object_entries.forEach((object)=>{
+    // console.log(value);
+        if (object.id === selectedData){
+            // succesfully pulled correct object from the samples dictionary
+            console.log(object);
+            // successfully returned the correct id from dropdown menu
+            console.log(selectedData);
+            new_x = object.sample_values.slice(0,10);
+            console.log(new_x);
+            new_y = object.otu_ids.slice(0,10);
+            console.log(new_y);
+            var sliced_labels = new_y.map(item=>`OTU ID:${item}`)
+            var trace1 = {
+                x: new_x,
+                y: sliced_labels,
+                type: "bar",
+                orientation: "h"
+            }
+        
+            var data = [trace1];
+            
+            var layout = {
+            height: 900,
+            width: 900,
+            };
+            
+              Plotly.newPlot("bar", data, layout);
+
+    };
+    // updatePlotly(x,y);    
+    });
+    });
+    };
+// function updatePlotly(x,y){
+//     Plotly.restyle("bar", trace1.x,trace1,y,[new_data]);
+// };     
+    // if (dataset == '941'){
+    //     var panel = d3.select("#sample-metadata");
+    //     var test_object = sample_metadata[1];
+    //     panel.html("");
+    //     Object.entries(test_object).forEach(([key,value])=>{
+    //         panel.append("p").text(`${key}:${value}`);
+
+        // if (dataset == sample_ids[1]){
+        // var panel = d3.select("#sample-metadata");
+        // var test_object = sample_metadata[1];
+        // panel.html("");
+        // Object.entries(test_object).forEach(([key,value])=>{
+        //     panel.append("p").text(`${key}:${value}`);
+    
+            // var bar = d3.select("bar");
+    // });
+    
+    // Plotly.restyle("bar")
+    // console.log(sample_object_entries);
+    // var bar = d3.select("bar");
 
     // Inside names_var: you have an id, nothing else
     // Inside samples_var: you have otu_id's , sample_values, otu_labels
